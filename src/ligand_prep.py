dfdae811ab1dc2adca5716e8ab6568edd1809b18
rmadeye <rafmadaj@gmail.com>
2020-10-19 11:41:30 +0200

    Beta version - smi input

--- src/ligand_prep.py
+++ src/ligand_prep.py
@@ -1,26 +1,27 @@
-import urllib.request
-
 import os, shutil
+from time import sleep
 #set preparation pathways
 prepare_ligand_path = '~/MGLTools-1.5.6/bin/pythonsh ~/MGLTools-1.5.6/MGLToolsPckgs/AutoDockTools/Utilities24/prepare_ligand4.py -A bonds_hydrogens -U nphs_lps -l'
 
 class LigandPreparer:
 
-    def __init__(self, ligand_file: str, dir: str):
-        self.ligand = ligand_file
+    def __init__(self, smile: str, dir: str):
+        self.ligand = smile
         self.tmpdir = dir
 
     def prepare_ligand(self):
-        url = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/{}/SDF'.format(self.ligand)
-        urllib.request.urlretrieve(url, self.tmpdir + '/{}'.format(self.ligand + '.sdf')) # downloads the file
-        os.system('obabel {} -O {} --gen3d'.format(
-            self.tmpdir + '/' + self.ligand + '.sdf', self.tmpdir + '/' + self.ligand + 'prep.pdb')
-        ) # generates 3d coords
+        with open(self.tmpdir+'/'+self.ligand+'.smi', 'w') as smile_input_file:
+            smile_input_file.write(self.ligand)
+
+        os.system('obabel "{}" -O "{}" --gen3d'.format(
+            self.tmpdir + '/' + self.ligand + '.smi', self.tmpdir + '/' + self.ligand + 'prep.pdb'))
+        # generates 3d coords
         os.system(
-            'obminimize -ff GAFF {} > {}'.format(
+            'obminimize -ff GAFF "{}" > "{}"'.format(
                 self.tmpdir + '/' + self.ligand + 'prep.pdb', self.tmpdir + '/' + self.ligand + '.pdb'
                                                  )
-        ) # minimizes using GAFF
-        os.system(prepare_ligand_path + '{}'.format(self.tmpdir + '/' + self.ligand + '.pdb'
+        )
+        #minimizes using GAFF
+        os.system(prepare_ligand_path + '"{}"'.format(self.tmpdir + '/' + self.ligand + '.pdb'
                                                     )) # adds charges, sets rotatable bonds
         shutil.move(self.ligand + '.pdbqt', self.tmpdir)
--- src/perform_docking.py
+++ src/perform_docking.py
@@ -40,7 +40,7 @@ class VinaDocker:
         print("Sizes of docking grid are as follows:"
               "x: {}, y: {}, z: {}".format(cent_x, cent_y, cent_z))
         os.system(
-            'vina --receptor {} --ligand {} --center_x {} --center_y {} --center_z {} --size_x {} --size_y {} --size_z {} --log {} --out {}'.format(
+            'vina --receptor {} --ligand "{}" --center_x {} --center_y {} --center_z {} --size_x {} --size_y {} --size_z {} --log "{}" --out "{}"'.format(
                 self.protein,
                 self.ligand,
                 cent_x,
